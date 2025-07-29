1b9815bc0a5fc0180897d9d15097353a52414ca2
PiyushDhamecha454 <piyushdhamecha327@gmail.com>
2025-07-29 19:21:14 +0530

    Integrating TailwindCSS w/Vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import GoogleLoginButton from './components/GoogleLoginButton';
 
 function App() {
   return (
     <div>
       <h1>Welcome</h1>
-      <GoogleLoginButton />
+      <GoogleLoginButton /> 
     </div>
   );
 }
